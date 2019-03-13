2a998e88873d08b47bc3c475452027bdbd42c761
flippinberger <bergercm@umich.edu>
2019-03-13 10:58:12 -0400

    Add model methods for inc interactive shell

--- todo.go
+++ todo.go
@@ -41,8 +41,51 @@ func AddTasks(todos []string) (*[]Task, error) {
 	return &taskSlice, nil
 }
 
+// AppendTasks will append 2 slices of tasks togeter
+func AppendTasks(tasks *[]Task, newTasks *[]Task) *[]Task {
+	newSlice := append(*tasks, *newTasks...)
+	return &newSlice
+}
+
+// DeleteTask will remove the task at index
+func DeleteTask(tasks *[]Task, index int) {
+	if len(*tasks) == 1 {
+		*tasks = nil
+	} else if index == len(*tasks)-1 {
+		*tasks = (*tasks)[:index]
+	} else {
+		*tasks = append((*tasks)[:index], (*tasks)[index+1:]...)
+	}
+}
+
+// Complete will mark a task as complete
+func (t *Task) Complete() {
+	t.Completed = true
+}
+
+// Uncomplete will mark a task uncomplete
+func (t *Task) Uncomplete() {
+	t.Completed = false
+}
+
+// AddNote adds the provided note to the task
+func (t *Task) AddNote(note string) {
+	t.Notes = append(t.Notes, note)
+}
+
+// DeleteNote will remove the note at index
+func (t *Task) DeleteNote(index int) {
+	if len(t.Notes) == 1 {
+		t.Notes = nil
+	} else if index == len(t.Notes)-1 {
+		t.Notes = t.Notes[:index]
+	} else {
+		t.Notes = append(t.Notes[:index], t.Notes[index+1:]...)
+	}
+}
+
 // Default string to be printed for a task
-func (t Task) String() string {
+func (t *Task) String() string {
 	//TODO Add some sort of notion of completion here at the beginning
 	info := "["
 
